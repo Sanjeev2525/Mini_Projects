4859c0c03eecc4a21e4ee7d507d1acc2c0b20698
Sanjeev <sanjeevganesh25@gmail.com>
2022-08-11 21:53:55 +0530

    Well you learned somethig today

---
+++ Notes app/app.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const bodyParser=require("body-parser");
+
+const app = express();
+
+app.set('view engine', 'ejs'); // use ejs as it view engine
+
+
+app.get("/",function(req,res) {
+
+	var today =  new Date();
+	var currentDay = today.getDay();
+	var day = "";
+	switch(currentDay){
+		case 0:
+			day="Sunday";
+			break;
+		case 1:
+			day="Monday";
+			break;
+		case 2:
+			day="Tuesday";
+			break;
+		case 3:
+			day="Wednesday";
+			break;
+		case 4:
+			day="Thursday";
+			break;
+		case 5:
+			day="Friday";
+			break;
+		case 6:
+			day="Saturday";
+			break;
+		default:
+		console.log("Error: currentDay is not valid something's wrong")
+	}
+res.render('list', {
+	WhichDay: day}) ;	
+});
+
+
+  
+
+app.listen(3000,function(){
+	console.log("Server is running in port 3000");
+});
