7ec7b929f273a64322f94e18e1030cc028b5d1b2
Sanjeev <sanjeevganesh25@gmail.com>
2022-08-15 13:42:17 +0530

    EJS Layout

--- Notes app/app.js
+++ Notes app/app.js
@@ -3,35 +3,50 @@ const bodyParser=require("body-parser");
 
 const app = express();
 
-var notes = ["Add your to do list in here"];
+let notes = ["Add your to do list in here"]; //use let instead of var since let is local and var is global
+let workItems=[];
+
 
 app.set('view engine', 'ejs'); // use ejs as it view engine
 
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(express.static("public"));
 app.get("/",function(req,res) {
 
-	var today =  new Date();
+	let today =  new Date();
 	
-	var options ={
+	let options ={
 		weekday: "long",
 		day: "numeric",
 		month: "long"
 	};
 
-	var day= today.toLocaleDateString("en-US",options); 
+	let day= today.toLocaleDateString("en-US",options); 
 
-	res.render('list', { WhichDay: day,
+	res.render("list", { listTitle: day,
 		                 newNotes:  notes  }) ; // reason why we add here is cant have two render stateemets
 
 });
-app.post("/",function(req,res){
-	var list=req.body.newNote;
-	notes.push(list);
-	res.redirect("/") //redirects
+app.post("/", function(req,res){
+	let list=req.body.inputednote;
+	console.log(req.body);
+	if(req.body.list==="Work"){
+		workItems.push(list);
+		res.redirect("/work")
+	}else{
+		notes.push(list);
+		res.redirect("/"); //redirects	
+	}
+	
 });
 
-		
+
+app.get("/work", function(req,res){
+	res.render("list", { listTitle: "Work List" ,
+		                 newNotes:  workItems  }) ;
+
+});		
+
 
 
 
